0f2f3dcc99e5a7b816ace022d3759a2291f1f119
virendrachaudhary06 <virendrachaudhary06@gmail.com>
2019-05-13 18:44:35 +0530

    reverted changes

--- functional-test/src/test/java/com/homeaway/DataPull/CronExpressionTest.java
+++ functional-test/src/test/java/com/homeaway/DataPull/CronExpressionTest.java
@@ -20,7 +20,6 @@ public class CronExpressionTest extends BaseTest {
     @Test
     public void cronExpressionTest() throws ParseException, InterruptedException {
         InputJson inputJson = new InputJson("Close-EMR-Cluster");
-
         EmrCluster emrCluster = new EmrCluster(inputJson);
         if (emrCluster.getClusterId() == null) {
             DataPull.start(inputJson);
