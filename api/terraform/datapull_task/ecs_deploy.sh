08d06ee3f457c96d1a3b5d7f314a2194a9f3384b
srigajjala <srigajjala@vrbo.com>
2020-05-22 14:40:22 -0500

    Adding HIVE as destination

--- api/terraform/datapull_task/ecs_deploy.sh
+++ api/terraform/datapull_task/ecs_deploy.sh
@@ -136,7 +136,6 @@ pwd
 docker run -e MAVEN_OPTS="-Xmx1024M -Xss128M -XX:MetaspaceSize=512M -XX:MaxMetaspaceSize=1024M -XX:+CMSClassUnloadingEnabled" --rm -v "${PWD}":/usr/src/mymaven -v "${PWD}/m2":/root/.m2 -w /usr/src/mymaven maven:alpine mvn clean install
 exitAfterFailure
 
-exit 0
 echo "Uploading core Jar to s3"
 
 docker run -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -e AWS_PROFILE -v "${PWD}":/data -v "${HOME}/.aws":"/root/.aws" garland/aws-cli-docker aws s3 cp /data/target/DataMigrationFramework-1.0-SNAPSHOT-jar-with-dependencies.jar "$jar_file_path"
