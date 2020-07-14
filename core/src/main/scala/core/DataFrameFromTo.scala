05fd8fdb72861c3d55cde8ea74d3311c2a710130
Marko Varghese <marko.varghese@gmail.com>
2020-07-14 18:57:31 -0500

    Resolve Issue #70 (#71)
    * hive support
    
    * hive
    
    * Adding HIVE as destination
    
    * Custom role creation
    
    * Custom role creation
    
    * Updates for Issue 64; plus simplified instructions for documentation
    
    * Simplified dockerised execution, updated changelog
    
    * Put back accidentally deleted documentation
    
    * Changes made for installation attempt to AWS
    
    * Removed redundant -x
    
    * Fixed changelog versioning and date
    
    * Updated changelog for #67
    
    * Revert "hive (#60)"
    
    This reverts commit 68478198b743aa4ebd26ed6151292fded7232b30.
    
    Changes to be committed:
    	modified:   core/pom.xml
    	deleted:    core/src/main/resources/Samples/Input_Sample_hive-to-filesystem.json
    	modified:   core/src/main/scala/core/DataFrameFromTo.scala
    	modified:   core/src/main/scala/core/Migration.scala
    
    * Tested reduced permission for EMR service role successfully
    
    * Updated to have API run on https
    
    * Removed changes made for debugging, updated changelog and documentation
    
    * Fixed incorrect documentation
    
    * Updated documentation and namings for custom EMR EC2 role
    
    * Updated logo
    
    Co-authored-by: srigajjala <srigajjala@vrbo.com>
    Co-authored-by: Marko Varghese <rovarghese@vrbo.com>

--- core/src/main/scala/core/DataFrameFromTo.scala
+++ core/src/main/scala/core/DataFrameFromTo.scala
@@ -23,8 +23,6 @@ import java.text.SimpleDateFormat
 import java.time.Instant
 import java.util
 import java.util.{Calendar, Properties, UUID}
-import javax.mail.internet.{InternetAddress, MimeMessage}
-import javax.mail.{Message, Session, Transport}
 
 import com.amazonaws.services.logs.model.{DescribeLogStreamsRequest, InputLogEvent, PutLogEventsRequest}
 import com.amazonaws.services.s3.model._
@@ -41,6 +39,8 @@ import com.mongodb.{MongoClient, MongoClientURI}
 import config.AppConfig
 import core.DataPull.jsonObjectPropertiesToMap
 import helper._
+import javax.mail.internet.{InternetAddress, MimeMessage}
+import javax.mail.{Message, Session, Transport}
 import org.apache.avro.Schema
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.hadoop.conf.Configuration
@@ -49,7 +49,6 @@ import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.jdbc.JdbcDialects
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import org.bson.Document
 import org.codehaus.jettison.json.JSONObject
@@ -1300,30 +1299,12 @@ class DataFrameFromTo(appConfig: AppConfig, pipeline : String) extends Serializa
     df.write.mode(savemode).options(jdbcOptions).jdbc(url, table, connectionProperties)
   }
 
-  def hiveToDataFrame(cluster: String,sparkSession: org.apache.spark.sql.SparkSession, dbtable: String, username:String,fetchsize:String): org.apache.spark.sql.DataFrame  = {
+  def hiveToDataFrame(table: String, sparkSession: SparkSession): org.apache.spark.sql.DataFrame = {
 
-    import org.apache.spark.sql.jdbc.JdbcDialect
+    sparkSession.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
 
-    val HiveDialect = new JdbcDialect {
-      override def canHandle(url: String): Boolean = url.startsWith("jdbc:hive2") || url.contains("hive2")
-
-      override def quoteIdentifier(colName: String): String =
-
-      { s"$colName" }
-
-    }
-
-    JdbcDialects.registerDialect(HiveDialect)
-
-    val jdbcDF = sparkSession.read
-      .format("jdbc")
-      .option("url", cluster)
-      .option("dbtable", dbtable)
-      .option("username",username)
-      // .option("fetchsize", fetchsize.toInt)
-      .load()
-
-    jdbcDF
+    val df = sparkSession.sql("Select * FROM " + table)
+    df
   }
 
   def dataFrameToHive(table: String, saveMode: String, df: org.apache.spark.sql.DataFrame): Unit = {
