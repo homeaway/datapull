26293fb65e85a51836a48efdeb79347c104a010c
Nirav <49956086+niravns@users.noreply.github.com>
2020-12-03 10:56:17 -0600

    Update core/src/main/scala/helper/Helper.scala
    Co-authored-by: Marko Varghese <marko.varghese@gmail.com>

--- core/src/main/scala/helper/Helper.scala
+++ core/src/main/scala/helper/Helper.scala
@@ -207,7 +207,7 @@ class Helper(appConfig: AppConfig) {
   }
 
   def buildTeradataURI(server: String, database: String, port: Option[Int],isWindowsAuthenticated:String): String = {
-    if(isWindowsAuthenticated.toBoolean)
+    "jdbc:teradata://" + server + "/" + (if (isWindowsAuthenticated) "LOGMECH=LDAP," else "") + "TYPE=FASTLOAD,DATABASE=" + database + ",TMODE=TERA,DBS_PORT=" + port.getOrElse(1025).toString
       {
         "jdbc:teradata://" + server + "/LOGMECH=LDAP,TYPE=FASTLOAD,DATABASE=" + database + ",TMODE=TERA,DBS_PORT=" + port.getOrElse(1025).toString
 
@@ -219,5 +219,4 @@ class Helper(appConfig: AppConfig) {
 }
 
 case class HttpResponse(ResponseCode: Int, ResponseBody: String)
-
 
