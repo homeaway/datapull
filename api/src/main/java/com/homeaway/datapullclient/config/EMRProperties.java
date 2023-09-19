ee2b971d82fe540426c8e8c11ddd6621e3adc035
srinugajjala <srinu.gajjala321@gmail.com>
2023-09-19 12:00:09 -0500

    Changing the default instance type to m6g.large

--- api/src/main/java/com/homeaway/datapullclient/config/EMRProperties.java
+++ api/src/main/java/com/homeaway/datapullclient/config/EMRProperties.java
@@ -48,10 +48,10 @@ public class EMRProperties {
     @Value("${instance_count:6}")
     private int instanceCount;
 
-    @Value("${master_type:m6g.xlarge}")
+    @Value("${master_type:m6g.large}")
     private String masterType;
 
-    @Value("${slave_type:m6g.xlarge}")
+    @Value("${slave_type:m6g.large}")
     private String slaveType;
 
     @Value("${service_role:emr_datapull_role}")
